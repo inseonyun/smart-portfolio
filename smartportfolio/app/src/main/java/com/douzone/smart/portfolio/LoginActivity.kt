7dbca90a64093faaf45788fd18ecbdf0096f1e63
inseonyun <yuninseon@naver.com>
2022-09-27 00:17:15 +0900

    Back Button 이벤트 추가

--- smartportfolio/app/src/main/java/com/douzone/smart/portfolio/LoginActivity.kt
+++ smartportfolio/app/src/main/java/com/douzone/smart/portfolio/LoginActivity.kt
@@ -2,10 +2,23 @@ package com.douzone.smart.portfolio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
+    // back button event
+    var backTime = 0L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    override fun onBackPressed() {
+        if(System.currentTimeMillis() - backTime > 3000) {
+            Toast.makeText(applicationContext, R.string.back_toast_message, Toast.LENGTH_SHORT).show()
+            backTime = System.currentTimeMillis()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
--- smartportfolio/app/src/main/java/com/douzone/smart/portfolio/MainActivity.kt
+++ smartportfolio/app/src/main/java/com/douzone/smart/portfolio/MainActivity.kt
@@ -2,11 +2,17 @@ package com.douzone.smart.portfolio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.douzone.smart.portfolio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
+
+    // back button event
+    var backTime = 0L
+
+    var fragment_home: Fragment_Home ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +20,21 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        binding.tvTf.text = "test"
+        fragment_home = Fragment_Home()
+
+        // frame layout
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_main, fragment_home!!)
+            .commit()
+
+    }
+
+    override fun onBackPressed() {
+        if(System.currentTimeMillis() - backTime > 3000) {
+            Toast.makeText(applicationContext, R.string.back_toast_message, Toast.LENGTH_SHORT).show()
+            backTime = System.currentTimeMillis()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
