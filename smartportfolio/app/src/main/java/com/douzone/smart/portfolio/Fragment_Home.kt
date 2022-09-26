49e5e651ef8d3fa01fc53060ac1331754885dae2
inseonyun <yuninseon@naver.com>
2022-09-27 00:16:50 +0900

    로그인 UI 수정 및 Fragment_Home 추가

---
+++ smartportfolio/app/src/main/java/com/douzone/smart/portfolio/Fragment_Home.kt
@@ -0,0 +1,26 @@
+package com.douzone.smart.portfolio
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.douzone.smart.portfolio.databinding.FragmentHomeBinding
+
+class Fragment_Home : Fragment() {
+    // ViewBinding
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+
+
+        return view
+    }
+}
