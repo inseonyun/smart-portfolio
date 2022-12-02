6a7d67d3e8c881561eeb79619640a09d82738b2a
inseonyun <yuninseon@naver.com>
2022-12-02 15:18:34 +0900

    feat: Messenger 형태 포트폴리오 구현
    1. Messenger 클래스 - 메신저 포트폴리오 데이터 클래스
    2. MessengerPortfolioAdapter - 메신저 포트폴리오 화면의 뷰 어댑터
    3. MessengerPortfolioDatabaseHelper - 메신저 포트폴리오 db에서 읽기 및 씀
    4. PortfolioMessengerViewHolder - 뷰페이저 메신저 뷰의 리스트뷰에 db에서의 정보를 통해 타임라인 어댑터를 연결함
    5. listview_portfolio_messenger 뷰 - 실질적으로 보여지는 메신저 뷰의 형태
    6. view_pager_messenger - 메신저 포트폴리오를 나열한 리스트뷰만 있음

---
+++ smartportfolio/app/src/main/java/com/douzone/smart/portfolio/data/Messenger.kt
@@ -0,0 +1,10 @@
+package com.douzone.smart.portfolio.data
+
+class Messenger (
+    var id: Int,
+    var name: String,
+    var title: String,
+    var contents: String,
+    var image: String?,
+    var url: String?
+)
