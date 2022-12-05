82ce86032151f6960486df3297883b837e1bd712
inseonyun <yuninseon@naver.com>
2022-12-05 23:45:55 +0900

    refactor: 로그인 화면 수정
    1. 앱 네임 이미지 fadeIn 추가
    2. 뷰 구성 수정
    3. EditText hint 추가 및 inputType, maxLength 설정

--- smartportfolio/app/src/main/java/com/douzone/smart/portfolio/LoginActivity.kt
+++ smartportfolio/app/src/main/java/com/douzone/smart/portfolio/LoginActivity.kt
@@ -3,6 +3,8 @@ package com.douzone.smart.portfolio
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AlphaAnimation
 import android.widget.Toast
 import com.douzone.smart.portfolio.databinding.ActivityLoginBinding
 
@@ -12,14 +14,28 @@ class LoginActivity : AppCompatActivity() {
     // back button event
     var backTime = 0L
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(getLayoutInflater())
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+        logoFadeIn()
         loginButtonEvent()
+    }
+
+    private fun View.hideAndShow(time : Long = 2000) {
+        visibility = View.INVISIBLE
+        startAnimation(
+            AlphaAnimation(0.0f, 1.0f).apply {
+                duration = time
+                fillAfter = true
+            }
+        )
+    }
+
+    private fun logoFadeIn() {
+        binding.ivAppName.hideAndShow()
     }
 
     override fun onBackPressed() {
