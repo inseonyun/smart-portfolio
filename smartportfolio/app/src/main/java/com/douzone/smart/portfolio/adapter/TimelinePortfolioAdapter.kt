f757628b5cb1fcab23004478620a572d82b219b7
inseonyun <yuninseon@naver.com>
2022-12-07 17:21:30 +0900

    refactor: 각 포트폴리오 adapter에서 context 값에 따라 이벤트 처리

--- smartportfolio/app/src/main/java/com/douzone/smart/portfolio/adapter/TimelinePortfolioAdapter.kt
+++ smartportfolio/app/src/main/java/com/douzone/smart/portfolio/adapter/TimelinePortfolioAdapter.kt
@@ -1,14 +1,18 @@
 package com.douzone.smart.portfolio.adapter
 
 import android.content.Context
+import android.content.DialogInterface
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import androidx.appcompat.app.AlertDialog
 import com.douzone.smart.portfolio.R
 import com.douzone.smart.portfolio.data.CircleType
 import com.douzone.smart.portfolio.data.Timeline
 import com.douzone.smart.portfolio.databinding.ListviewPortfolioTimelineBinding
+import com.douzone.smart.portfolio.db.TimelinePortfolioDatabaseHelper
 import java.time.LocalDate
 
 class TimelinePortfolioAdapter(val context: Context, var items: ArrayList<Timeline>): BaseAdapter() {
@@ -40,6 +44,27 @@ class TimelinePortfolioAdapter(val context: Context, var items: ArrayList<Timeli
             CircleType.GREEN -> binding.ivTimelineCircle.setImageResource(R.drawable.img_green_timeline_circle)
             CircleType.ORANGE -> binding.ivTimelineCircle.setImageResource(R.drawable.img_orange_timeline_circle)
             else -> binding.ivTimelineCircle.setImageResource(R.drawable.img_blue_timeline_circle)
+        }
+
+        // 불려온 context가 delete라면 삭제 이벤트 처리
+        if(context.toString().contains("DeletePortfolio")) {
+            binding.cardView.setOnClickListener {
+                AlertDialog.Builder(context).run {
+                    setTitle("포트폴리오 삭제")
+                    setMessage("해당 포트폴리오를 삭제할까요?")
+                    setPositiveButton("예", DialogInterface.OnClickListener { dialogInterface, _ ->
+                        val dbHelper = TimelinePortfolioDatabaseHelper(context)
+                        dbHelper.deletePortfolio(items[p0].id)
+                        this@TimelinePortfolioAdapter.items.removeAt(p0)
+                        this@TimelinePortfolioAdapter.notifyDataSetChanged()
+                        dialogInterface.dismiss()
+                    })
+                    setNegativeButton("취소", DialogInterface.OnClickListener { dialogInterface, _ ->
+                        dialogInterface.dismiss()
+                    })
+                    show()
+                }
+            }
         }
 
         // 이미지 넣음
