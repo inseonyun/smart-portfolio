be5ebb31b62137eaa49bfb25493f7bc3fe2ae205
inseonyun <yuninseon@naver.com>
2022-12-09 23:43:02 +0900

    feat: Dialog View 및 어댑터 구현

---
+++ smartportfolio/app/src/main/java/com/douzone/smart/portfolio/adapter/DialogAddPortfolioListViewAdapter.kt
@@ -0,0 +1,54 @@
+package com.douzone.smart.portfolio.adapter
+
+import android.content.Context
+import android.content.DialogInterface
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import androidx.appcompat.app.AlertDialog
+import com.douzone.smart.portfolio.MainActivity
+import com.douzone.smart.portfolio.`interface`.DialogUserOnItemClick
+import com.douzone.smart.portfolio.data.AddDelete
+import com.douzone.smart.portfolio.data.User
+import com.douzone.smart.portfolio.databinding.ListviewDialogUserBinding
+
+class DialogAddPortfolioListViewAdapter(val context: Context, var items: ArrayList<User>, private val listener: DialogUserOnItemClick) : BaseAdapter() {
+
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+        val binding = ListviewDialogUserBinding.inflate(LayoutInflater.from(context))
+
+        binding.tvUserName.text = items[p0].name
+
+        binding.cvUser.setOnClickListener {
+            (context as MainActivity)
+            // 추가 액티비티로 이동
+            AlertDialog.Builder(context).run {
+                setTitle("포트폴리오 추가")
+                setMessage("${items[p0].name}의 포트폴리오를 추가하시겠습니까?")
+                setPositiveButton("예", DialogInterface.OnClickListener { dialogInterface, _ ->
+                    listener.onClick(items[p0].name, items[p0].viewType, AddDelete.ADD)
+                    dialogInterface.dismiss()
+                })
+                setNegativeButton("아니오", DialogInterface.OnClickListener { dialogInterface, _ ->
+                    dialogInterface.dismiss()
+                })
+                show()
+            }
+        }
+
+        return binding.root
+    }
+
+    override fun getCount(): Int {
+        return items.size
+    }
+
+    override fun getItem(p0: Int): Any {
+        return items[p0]
+    }
+
+    override fun getItemId(p0: Int): Long {
+        return 0
+    }
+}
