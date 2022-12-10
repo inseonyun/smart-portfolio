3f892f5df499e900ff5893017a5bba954d544ea7
inseonyun <yuninseon@naver.com>
2022-12-10 17:09:29 +0900

    refactor: User 클래스 데이터 수정에 따른 DBHelper 수정

--- smartportfolio/app/src/main/java/com/douzone/smart/portfolio/db/UserDatabaseHelper.kt
+++ smartportfolio/app/src/main/java/com/douzone/smart/portfolio/db/UserDatabaseHelper.kt
@@ -3,12 +3,14 @@ package com.douzone.smart.portfolio.db
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.database.sqlite.SQLiteStatement
 import com.douzone.smart.portfolio.data.User
 
 class UserDatabaseHelper(context: Context) : SQLiteOpenHelper(context, "userDatabase.db", null, 1) {
     override fun onCreate(db: SQLiteDatabase?) {
         val sql = "CREATE TABLE if not exists user(" +
                 "name text primary key,"+
+                "profileImage blob,"+
                 "viewType integer);"
         db?.execSQL(sql)
     }
@@ -20,9 +22,13 @@ class UserDatabaseHelper(context: Context) : SQLiteOpenHelper(context, "userData
     }
 
     fun insertData(data: User) {
-        val query = "INSERT INTO user('name', 'viewType') values('${data.name}', '${data.viewType}');"
         val db = this.writableDatabase
-        db.execSQL(query)
+        val query = "INSERT INTO user('name', 'profileImage', 'viewType') values(?, ?, ${data.viewType});"
+        val p = db.compileStatement(query)
+        p.bindString(1, data.name)
+        p.bindBlob(2, data.profileImage)
+        p.execute()
+        p.close()
         db.close()
     }
 
@@ -34,9 +40,12 @@ class UserDatabaseHelper(context: Context) : SQLiteOpenHelper(context, "userData
     }
 
     fun updateData(data: User) {
-        val query = "UPDATE FROM user set name = '${data.name}', viewType = '${data.viewType}'"
         val db = this.writableDatabase
-        db.execSQL(query)
+        val query = "UPDATE user set name = '${data.name}', profileImage = ?, viewType = '${data.viewType}' WHERE name = '${data.name}'"
+        val p = db.compileStatement(query)
+        p.bindBlob(1, data.profileImage)
+        p.execute()
+        p.close()
         db.close()
     }
 
@@ -49,13 +58,23 @@ class UserDatabaseHelper(context: Context) : SQLiteOpenHelper(context, "userData
         return false
     }
 
+    fun selecetUser(name: String): User? {
+        val db = this.readableDatabase
+
+        val cursor = db.rawQuery("SELECT * FROM user WHERE name = '$name'", null)
+        while(cursor.moveToNext()) {
+            return User(cursor.getString(0), cursor.getBlob(1), cursor.getInt(2))
+        }
+        return null
+    }
+
     fun selectData(): ArrayList<User> {
         val db = this.readableDatabase
         val result = ArrayList<User>()
 
         val cursor = db.rawQuery("SELECT * FROM user", null)
         while(cursor.moveToNext()) {
-            val rowData = User(cursor.getString(0), cursor.getInt(1))
+            val rowData = User(cursor.getString(0), cursor.getBlob(1), cursor.getInt(2))
             result.add(rowData)
         }
 
